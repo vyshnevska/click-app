2795c29ac6433ce7deb6cd2769c3b9f4eb335c0c
Nina Vyshnevska <vyshnevska.n@gmail.com>
2015-06-26 02:04:25 +0300

    Create a new add; list existing ads

--- click_app_server.rb
+++ click_app_server.rb
@@ -40,18 +40,41 @@ get '/ad' do
 end
 
 get '/list' do
+  @title = 'List of Ads'
+  @ads = Ad.all(order: [:created_at.desc])
+  erb :list
 end
 
 get '/new' do
+  @title = "New Ad"
+  erb :new
 end
 
 post '/create' do
+  @ad = Ad.new(params[:ad])
+  @ad.content_type = params[:image][:type]
+  @ad.size = File.size(params[:image][:tempfile])
+  if @ad.save
+    path = File.join(Dir.pwd, "/public/ads", @ad.filename)
+    File.open(path, 'wb') do |f|
+      f.write(params[:image][:tempfile].read)
+    end
+    redirect "/show/#{@ad.id}"
+  else
+    redirect('/list')
+  end
 end
 
 get '/delete/:id' do
 end
 
 get '/show/:id' do
+  @ad = Ad.get(params[:id])
+  if @ad
+    erb :show
+  else
+    redirect('/list')
+  end
 end
 
 get '/click/:id' do
